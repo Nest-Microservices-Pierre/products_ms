92efdaa50f7c492288832268f1c581bea46df316
pierre <pierreaqv@hotmail.com>
2024-05-17 09:05:44 -0500

    products

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,10 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import {
-  Injectable,
-  Logger,
-  NotFoundException,
-  OnModuleInit,
-} from '@nestjs/common';
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaClient } from '@prisma/client';
